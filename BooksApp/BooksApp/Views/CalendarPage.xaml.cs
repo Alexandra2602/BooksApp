4b089b733ad6343b285a5a258e7a00eefee127ab
marian alexandra <marianalexandra2602@yahoo.com>
2022-03-16 14:41:38 +0200

    c

--- BooksApp/BooksApp/Views/CalendarPage.xaml.cs
+++ BooksApp/BooksApp/Views/CalendarPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamForms.Controls;
 
 namespace BooksApp.Views
 {
@@ -15,6 +16,38 @@ namespace BooksApp.Views
         public CalendarPage()
         {
             InitializeComponent();
+
+
+            XamForms.Controls.Calendar calendar = new XamForms.Controls.Calendar();
+        
+        }
+         async void Calendar_DateClicked(Object sender, XamForms.Controls.DateTimeEventArgs e)
+        {
+            string result = await DisplayPromptAsync("SpecialEvent", "Create a new Event:");
+
+            if (result == null)
+            {
+                return;
+            }
+            var specialDates = calendar.SpecialDates;
+
+            SpecialDate newDate = new SpecialDate(e.DateTime)
+            {
+                Selectable = true,
+                BackgroundPattern = new BackgroundPattern(1)
+                {
+                    Pattern = new List<Pattern>
+                     {
+                         new Pattern { WidthPercent = 1f, HightPercent = 0.6f, Color = Color.Transparent },
+                         new Pattern{ WidthPercent = 1f, HightPercent = 0.4f, Color = Color.Transparent, Text = result, TextColor=Color.Black, TextSize=11, TextAlign=TextAlign.Middle},
+                     }
+                }
+            };
+            specialDates.Add(newDate);
+
+            calendar.SpecialDates = specialDates;
+            calendar.ForceRedraw();
+
         }
         async void Top_Clicked(object sender, EventArgs e)
         {
