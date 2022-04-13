7a7e630f97e1915e5c4191b6b306811efa884b48
marian alexandra <marianalexandra2602@yahoo.com>
2022-04-13 21:22:08 +0300

    i

--- BookAPI/Models/User.cs
+++ BookAPI/Models/User.cs
@@ -6,15 +6,14 @@ using System.Threading.Tasks;
 namespace BookAPI.Models
 {
     public class User
-    {
-        [PrimaryKey, AutoIncrement]
+    {   
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
 
         public string Email { get; set; }
         public string Password { get; set; }
-        [MaxLength(10)]
+        
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
 
